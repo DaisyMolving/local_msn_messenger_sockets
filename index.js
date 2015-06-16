acd16fb228ecf60fb083169586c5cee06dbe3fdc
Daisy Molving <daisymolving@gmail.com>
2015-06-16 18:27:17 +0100

    set express function to look at views folder and ejs files along with get request to show index page

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ var http = require('http');
 server = http.createServer(app); // Create an HTTP server.
 server.listen(process.env.PORT || 3000); // Listen on the default port, or on 4000 if there's not one.
 
+ app.set('views', './views');
+ app.set('view engine', 'ejs');
+ app.use(express.static(__dirname + '/public'));
+
+ app.get('/', function(req, res){
+  res.render('index')
+ })
